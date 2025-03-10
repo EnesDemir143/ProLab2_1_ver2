d4b8d933bb19d2fdd64fd89f831d11252ddcba4d
EnesDemir143 <enesd41@outlook.com>
2025-03-11 00:13:30 +0300

    Stations classları tamam.

--- src/main/java/org/transportationroutecalculation/prolab2_1_ver2/MainClasses/Stations.java
+++ src/main/java/org/transportationroutecalculation/prolab2_1_ver2/MainClasses/Stations.java
@@ -12,6 +12,46 @@ public abstract class Stations {
     private ArrayList<NextStation> next_stations;
     private ArrayList<Transfer> transfer;
 
+    public Stations(String stationID, String stationType, Float lat, Float lon, Boolean is_last_station, ArrayList<NextStation> next_stations, ArrayList<Transfer> transfer) {
+        this.stationID = stationID;
+        this.stationType = stationType;
+        this.lat = lat;
+        this.lon = lon;
+        this.is_last_station = is_last_station;
+        this.next_stations = next_stations;
+        this.transfer = transfer;
+    }
+
+    public Boolean getIs_last_station() {
+        return is_last_station;
+    }
+
+    public double[] getLocation() {
+        return new double[]{lat, lon};
+    }
+
+    public String getStationID() {
+        return stationID;
+    }
+
+    public String getStationType() {
+        return stationType;
+    }
+}
+
+class BusStation extends Stations {
+
+    public BusStation(String stationID, String stationType, Float lat, Float lon, Boolean is_last_station, ArrayList<NextStation> next_stations, ArrayList<Transfer> transfer) {
+        super(stationID, stationType, lat, lon, is_last_station, next_stations, transfer);
+    }
+}
+
+
+class TramsStation extends Stations {
+
+    public TramsStation(String stationID, String stationType, Float lat, Float lon, Boolean is_last_station, ArrayList<NextStation> next_stations, ArrayList<Transfer> transfer) {
+        super(stationID, stationType, lat, lon, is_last_station, next_stations, transfer);
+    }
 }
 
 
@@ -36,4 +76,9 @@ class Transfer{
     private Float transfer_amount;
     private Integer transfer_time;
 
+    public Transfer(Stations transfer_station, Float transfer_amount, Integer transfer_time) {
+        this.transfer_station = transfer_station;
+        this.transfer_amount = transfer_amount;
+        this.transfer_time = transfer_time;
+    }
 }
