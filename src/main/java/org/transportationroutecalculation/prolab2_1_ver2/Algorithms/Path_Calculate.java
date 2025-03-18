7ea06ea1d1d5093b80d9cfa5b4a554c9110d747e
EnesDemir143 <enesd41@outlook.com>
2025-03-19 00:08:56 +0300

    Path hesaplamadaki duplicate kodlardan kurtulduk.

--- src/main/java/org/transportationroutecalculation/prolab2_1_ver2/Algorithms/Path_Calculate.java
+++ src/main/java/org/transportationroutecalculation/prolab2_1_ver2/Algorithms/Path_Calculate.java
@@ -15,7 +15,9 @@ import org.transportationroutecalculation.prolab2_1_ver2.HelperClasses.Controlle
 import org.transportationroutecalculation.prolab2_1_ver2.HelperClasses.NearestStations.FindNearestStation;
 import org.transportationroutecalculation.prolab2_1_ver2.MainClasses.StationTypes.Stations;
 
+import java.awt.geom.Point2D;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 @Service
@@ -48,85 +50,94 @@ public class Path_Calculate {
                 .collect(Collectors.toList());
     }
 
+    public void last_station_to_end(@RequestBody RequestData frontend_data, Path path){
+
+        double[] lastPoint = path.path().getLast();
+        Point2D.Double point_end = new Point2D.Double(lastPoint[0], lastPoint[1]);
+
+        Boolean walk_control_end =(walkingController.can_proceed(point_end, frontend_data.getTargetLocation().getLocation())).getKey();
+        double distance_end = (walkingController.can_proceed(point_end, frontend_data.getTargetLocation().getLocation())).getValue();
+
+        if (walk_control_end){
+            path.path().addLast(new double[]{frontend_data.getTargetLocation().getLocation().getX(), frontend_data.getTargetLocation().getLocation().getY()});
+        }
+        else{
+            double taxi_price = data.getTaxi().calculate_price(distance_end);
+            path.path().addLast(new double[]{frontend_data.getTargetLocation().getLocation().getX(), frontend_data.getTargetLocation().getLocation().getY()});
+            path.distance().set(path.distance().get() + distance_end);
+            path.amount().set(path.amount().get() + taxi_price);
+        }
+
+    }
+
+    public void start_to_end(@RequestBody RequestData frontend_data, Path path){
+
+        double[] startPoint = path.path().getLast();
+        Point2D.Double point_start = new Point2D.Double(startPoint[0], startPoint[1]);
+
+        Boolean walk_control_start = (walkingController.can_proceed(frontend_data.getCurrentLocation().getLocation(), point_start)).getKey();
+        double distance_start = (walkingController.can_proceed(frontend_data.getCurrentLocation().getLocation(), point_start)).getValue();
+        System.out.println("Distance nearest: " + distance_start);
+
+
+        if (walk_control_start){
+            path.path().addFirst(new double[]{frontend_data.getCurrentLocation().getLocation().getX(), frontend_data.getCurrentLocation().getLocation().getY()});
+        }
+        else{
+            double taxi_price = data.getTaxi().calculate_price(distance_start);
+
+            path.path().addFirst(new double[]{frontend_data.getCurrentLocation().getLocation().getX(), frontend_data.getCurrentLocation().getLocation().getY()});
+            path.distance().set(path.distance().get() + distance_start);
+            path.amount().set(path.amount().get() + taxi_price);
+        }
+    }
+
+    public HashMap<String, List<Route>> route_concat(@RequestBody RequestData frontend_data, Stations startStation, Stations endStation){
+
+        HashMap <String, List<Route>> backEndReturn= new HashMap<>();
+
+        double[] firststation = {frontend_data.getCurrentLocation().getLocation().getX(), frontend_data.getCurrentLocation().getLocation().getY()};
+        double[] laststation = {frontend_data.getTargetLocation().getLocation().getX(), frontend_data.getTargetLocation().getLocation().getY()};
+
+
+        Path path_for_time = aStar.findShortestPaths(startStation, endStation, Metric.TIME);
+        start_to_end(frontend_data, path_for_time);
+        last_station_to_end(frontend_data, path_for_time);
+        System.out.println("Time: " + path_for_time.time());
+
+        Path path_for_distance = aStar.findShortestPaths(startStation, endStation, Metric.DISTANCE);
+        start_to_end(frontend_data, path_for_distance);
+        last_station_to_end(frontend_data, path_for_distance);
+        System.out.println("Distance: " + path_for_distance.distance());
+
+        Path path_for_amount = aStar.findShortestPaths(startStation, endStation, Metric.AMOUNT);
+        start_to_end(frontend_data, path_for_amount);
+        last_station_to_end(frontend_data, path_for_amount);
+        System.out.println("Amount: " + path_for_amount.amount());
+
+
+
+        List<Route> routes = List.of(
+                new Route(convertPathToCoords(path_for_time.path()), new AtomicReference<>(path_for_time.distance().get()), new AtomicReference<>(path_for_time.time().get()), new AtomicReference<>(path_for_time.amount().get()), "Süre (Time)"),
+                new Route(convertPathToCoords(path_for_distance.path()), new AtomicReference<>(path_for_distance.distance().get()), new AtomicReference<>(path_for_distance.time().get()), new AtomicReference<>(path_for_distance.amount().get()), "Mesafe (Distance)"),
+                new Route(convertPathToCoords(path_for_amount.path()), new AtomicReference<>(path_for_amount.distance().get()), new AtomicReference<>(path_for_amount.time().get()), new AtomicReference<>(path_for_amount.amount().get()), "Tutar (Amount)")
+        );
+
+        backEndReturn.put("routes", routes);
+
+        return backEndReturn;
+    }
+
     public HashMap<String, List<Route>> path_calculate(@RequestBody RequestData frontend_data){
+
+        HashMap<String, List<Route>> backEndReturn;
 
         Stations startStation = findNearestStation.find_nearest_station(frontend_data.getCurrentLocation()).getFirst().stations();
         System.out.println("Start station: " + startStation.getStationID());
         Stations endStation = findNearestStation.find_nearest_station(frontend_data.getTargetLocation()).getFirst().stations();
         System.out.println("End station: " + endStation.getStationID());
 
-        HashMap <String, List<Route>> backEndReturn= new HashMap<>();
-
-        Boolean walk_control =(walkingController.can_proceed(frontend_data.getCurrentLocation().getLocation(),startStation.getLocation())).getKey();
-        double distance = (walkingController.can_proceed(frontend_data.getCurrentLocation().getLocation(),startStation.getLocation())).getValue();
-        System.out.println("Distance nearest: " + distance);
-
-
-        double[] firststation = {frontend_data.getCurrentLocation().getLocation().getX(), frontend_data.getCurrentLocation().getLocation().getY()};
-        double[] laststation = {frontend_data.getTargetLocation().getLocation().getX(), frontend_data.getTargetLocation().getLocation().getY()};
-
-        if (walk_control){
-            Path path_for_time = aStar.findShortestPaths(startStation, endStation, Metric.TIME);
-            path_for_time.path().addFirst(firststation);
-            path_for_time.path().addLast(laststation);
-            System.out.println("Time: " + path_for_time.time());
-
-            Path path_for_distance = aStar.findShortestPaths(startStation, endStation, Metric.DISTANCE);
-            path_for_distance.path().addFirst(firststation);
-            path_for_distance.path().addLast(laststation);
-            System.out.println("Distance: " + path_for_distance.distance());
-
-            Path path_for_amount = aStar.findShortestPaths(startStation, endStation, Metric.AMOUNT);
-            path_for_amount.path().addFirst(firststation);
-            path_for_amount.path().addLast(laststation);
-            System.out.println("Amount: " + path_for_amount.amount());
-
-            List<Route> routes = List.of(
-                    new Route(convertPathToCoords(path_for_time.path()), path_for_time.distance().get(), path_for_time.time().get(), path_for_time.amount().get(), "Süre (Time)"),
-                    new Route(convertPathToCoords(path_for_distance.path()), path_for_distance.distance().get(), path_for_distance.time().get(), path_for_distance.amount().get(), "Mesafe (Distance)"),
-                    new Route(convertPathToCoords(path_for_amount.path()), path_for_amount.distance().get(), path_for_amount.time().get(), path_for_amount.amount().get(), "Tutar (Amount)")
-            );
-
-            backEndReturn.put("routes", routes);
-        }
-
-        else{
-            double taxi_price = data.getTaxi().calculate_price(distance);
-
-            Path path_for_time = aStar.findShortestPaths(startStation, endStation, Metric.TIME);
-            path_for_time.path().addFirst(firststation);
-            path_for_time.path().addLast(laststation);
-            path_for_time.amount().set(path_for_time.amount().get() + taxi_price);
-            path_for_time.distance().set(path_for_time.distance().get() + distance);
-            System.out.println("Time: " + path_for_time.time());
-
-            Path path_for_distance = aStar.findShortestPaths(startStation, endStation, Metric.DISTANCE);
-            path_for_distance.path().addFirst(firststation);
-            path_for_distance.path().addLast(laststation);
-            path_for_distance.amount().set(path_for_distance.amount().get() + taxi_price);
-            path_for_distance.distance().set(path_for_distance.distance().get() + distance);
-
-            System.out.println("Distance: " + path_for_distance.distance());
-
-            Path path_for_amount = aStar.findShortestPaths(startStation, endStation, Metric.AMOUNT);
-            path_for_amount.path().addFirst(firststation);
-            path_for_amount.path().addLast(laststation);
-            path_for_amount.amount().set(path_for_amount.amount().get() + taxi_price);
-            path_for_amount.distance().set(path_for_amount.distance().get() + distance);
-
-            System.out.println("Amount: " + path_for_amount.amount());
-
-            List<Route> routes = List.of(
-                    new Route(convertPathToCoords(path_for_time.path()), path_for_time.distance().get(), path_for_time.time().get(), path_for_time.amount().get(), "Süre (Time)"),
-                    new Route(convertPathToCoords(path_for_distance.path()), path_for_distance.distance().get(), path_for_distance.time().get(), path_for_distance.amount().get(), "Mesafe (Distance)"),
-                    new Route(convertPathToCoords(path_for_amount.path()), path_for_amount.distance().get(), path_for_amount.time().get(), path_for_amount.amount().get(), "Tutar (Amount)")
-            );
-
-            backEndReturn.put("routes", routes);
-
-        }
-
-
+        backEndReturn = route_concat(frontend_data, startStation, endStation);
 
         return backEndReturn;
     }
--- src/main/java/org/transportationroutecalculation/prolab2_1_ver2/Algorithms/Route.java
+++ src/main/java/org/transportationroutecalculation/prolab2_1_ver2/Algorithms/Route.java
@@ -2,5 +2,6 @@ package org.transportationroutecalculation.prolab2_1_ver2.Algorithms;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
-public record Route(List<Map<String, Double>> path, double distance, int duration, double price, String bestFor) {}
+public record Route(List<Map<String, Double>> path, AtomicReference<Double> distance,AtomicReference<Integer> time, AtomicReference<Double> amount, String bestFor) {}
