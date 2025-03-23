831cc38aada530b899b76f1dc6b9ee1b3afcf716
EnesDemir143 <enesd41@outlook.com>
2025-03-24 02:33:46 +0300

    Birkaç kod tekrarı önüne geçildiç

--- src/main/java/org/transportationroutecalculation/prolab2_1_ver2/HelperClasses/PathCalculateHelp/RouteConcat.java
+++ src/main/java/org/transportationroutecalculation/prolab2_1_ver2/HelperClasses/PathCalculateHelp/RouteConcat.java
@@ -11,6 +11,7 @@ import org.transportationroutecalculation.prolab2_1_ver2.Algorithms.ShortestPath
 import org.transportationroutecalculation.prolab2_1_ver2.Algorithms.ShortestPaths.ShortestPaths;
 import org.transportationroutecalculation.prolab2_1_ver2.MainClasses.StationTypes.Stations;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,30 +42,32 @@ public class RouteConcat {
 
         HashMap <String, List<Route>> backEndReturn= new HashMap<>();
 
+        List<Path > paths = new ArrayList<>();
+        List<Route> routes = new ArrayList<>();
+
         Path path_for_time = shortestPaths.findShortestPaths(startStation, endStation, Metric.TIME);
-        System.out.println("Time: " + path_for_time.time());
-        beforeAndAfterTheStations.start_to_end(frontend_data, path_for_time);
-        beforeAndAfterTheStations.last_station_to_end(frontend_data, path_for_time);
-
+        paths.add(path_for_time);
         Path path_for_distance = shortestPaths.findShortestPaths(startStation, endStation, Metric.DISTANCE);
-        System.out.println("Distance: " + path_for_distance.distance());
-
-        beforeAndAfterTheStations.start_to_end(frontend_data, path_for_distance);
-        beforeAndAfterTheStations.last_station_to_end(frontend_data, path_for_distance);
-
+        paths.add(path_for_distance);
         Path path_for_amount = shortestPaths.findShortestPaths(startStation, endStation, Metric.AMOUNT);
-        System.out.println("Amount: " + path_for_amount.amount());
-        beforeAndAfterTheStations.start_to_end(frontend_data, path_for_amount);
-        beforeAndAfterTheStations.last_station_to_end(frontend_data, path_for_amount);
+        paths.add(path_for_amount);
+
+        for (Path path : paths) {
+            if (path == null) {
+                System.err.println("RouteConcat: Path bulunamadı!");
+                return null;
+            }
+            System.out.println("Distance: " + path.distance());
+            System.out.println("Time: " + path.time());
+            System.out.println("Amount: " + path.amount());
+            beforeAndAfterTheStations.start_to_end(frontend_data, path);
+            beforeAndAfterTheStations.last_station_to_end(frontend_data, path);
+
+            routes.add(new Route(convertPathToCoords(path.path()), new AtomicReference<>(path.distance().get()), new AtomicReference<>(path.time().get()), new AtomicReference<>(path.amount().get()), path.best_for()));
+        }
 
         Path taxi_path = taxiPath.justTaxi(frontend_data);
-
-        List<Route> routes = List.of(
-                new Route(convertPathToCoords(path_for_time.path()), new AtomicReference<>(path_for_time.distance().get()), new AtomicReference<>(path_for_time.time().get()), new AtomicReference<>(path_for_time.amount().get()), path_for_time.best_for()),
-                new Route(convertPathToCoords(path_for_distance.path()), new AtomicReference<>(path_for_distance.distance().get()), new AtomicReference<>(path_for_distance.time().get()), new AtomicReference<>(path_for_distance.amount().get()), path_for_distance.best_for()),
-                new Route(convertPathToCoords(path_for_amount.path()), new AtomicReference<>(path_for_amount.distance().get()), new AtomicReference<>(path_for_amount.time().get()), new AtomicReference<>(path_for_amount.amount().get()), path_for_time.best_for()),
-                new Route(convertPathToCoords(taxi_path.path()), new AtomicReference<>(taxi_path.distance().get()), new AtomicReference<>(taxi_path.time().get()), new AtomicReference<>(taxi_path.amount().get()), taxi_path.best_for())
-        );
+        routes.add(new Route(convertPathToCoords(taxi_path.path()), new AtomicReference<>(taxi_path.distance().get()), new AtomicReference<>(taxi_path.time().get()), new AtomicReference<>(taxi_path.amount().get()), taxi_path.best_for()));
 
         backEndReturn.put("routes", routes);
 
